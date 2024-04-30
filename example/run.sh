0973dbe4a7150b924972c74ea0c8da87f19a6942
bronsonj98 <bronsonj@login3.hoffman2.idre.ucla.edu>
2024-04-30 08:08:33 -0700

    clean up

--- example/run.sh
+++ example/run.sh
@@ -1,13 +1,5 @@
-
-dir=/u/home/b/bronsonj/project-sriram/sumrhe
-src=${dir}/src
-demo=${dir}/data
-
-python3 ${src}/sumrhe.py --pheno ${demo}/sumstats/10ksums_0.25_1.0/sim_0.sumstat \
-                  --bim ${dir}/10k_split.bim \
-                  --rhe ${demo}/rhe_trace_output_281k \
-                  --save-trace ${demo}/sumtrace/trace_281k_ref \
-                  --out ./out
-                  
+python3 ../src/sumrhe.py --pheno ./sim_50k_h_0.25_p_0.01.sumstat \
+                  --bim ./10k_split.bim \
+                  --trace ./ref241k_sumtrace.tr  
 
 
