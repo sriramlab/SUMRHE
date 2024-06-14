bd854e10fefe72f16a7b579ebb960b2c40ae4825
bronsonj98 <bronsonj@login1.hoffman2.idre.ucla.edu>
2024-06-14 15:28:11 -0700

    updated example script/files

--- example/run.sh
+++ example/run.sh
@@ -1,5 +1,5 @@
 python3 ../src/sumrhe.py --pheno ./sim_50k_h_0.25_p_0.01.sumstat \
-                  --bim ./10k_split.bim \
-                  --trace ./ref241k_sumtrace.tr  
-
-
+                  --ldscores ./double_uniform_0.2_ldscores.trunc.npy \
+                  --annot ./double_uniform_0.2.txt \
+                  --verbose \
+                  --njack 1000
